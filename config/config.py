f5185bb8955d6a6a77f46d3bfd01d4844e449206
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-02-18 16:36:19 -0500

    prefectored

--- config/config.py
+++ config/config.py
@@ -16,7 +16,7 @@ class Config:
     SEMGREP_CONFIG_DIR = os.getenv("SEMGREP_CONFIG_DIR", "./config/semgrep")
 
     SEMGREP_RULESETS = os.getenv("SEMGREP_RULESETS", f"{os.environ['HOME']}/.semgrep")
-    TRIVY_CACHE_DIR = os.getenv("TRIVY_CACHE_DIR", f"{os.environ['HOME']}/.cache/trivy/db")
+    TRIVY_CACHE_DIR = os.getenv("TRIVY_CACHE_DIR", f"{os.environ['HOME']}/.cache/trivy")
     GRYPE_CACHE_DIR = os.getenv("GRYPE_CACHE_DIR", f"{os.environ['HOME']}/.cache/grype/db")
 
     BITBUCKET_HOSTNAME = os.getenv("BITBUCKET_HOSTNAME", "bitbucket.org")
