06ee06ee94443a46355645a269dc0ba1b2b77233
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-02-21 13:21:59 -0500

    prefectored

---
+++ flows/chained.py
@@ -0,0 +1,29 @@
+from prefect import flow
+import asyncio
+from flows.standards import standards_assessment_flow
+from flows.fundamentals import fundamental_metrics_flow
+from flows.vulnerabilities import vulnerabilities_flow
+from flows.components import component_patterns_flow
+from modular.shared.utils import generate_main_flow_run_name
+
+
+@flow(name="Chained Main Flow", flow_run_name=generate_main_flow_run_name)
+async def chained_flow(payload: dict):
+    print("Starting Chained Main Flow...")
+
+    await fundamental_metrics_flow(payload)
+    await component_patterns_flow(payload)
+    await standards_assessment_flow(payload)
+    await vulnerabilities_flow(payload)
+
+    print("Chained Main Flow completed.")
+
+# For testing purposes.
+if __name__ == "__main__":
+    example_payload = {
+        "payload": {
+            "host_name": ["github.com"],
+            "activity_status": ["ACTIVE"]
+        }
+    }
+    asyncio.run(chained_flow(example_payload))
