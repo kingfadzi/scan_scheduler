29b5dfafec19ff84a360cea3d4154818b2584a2c
kingfadzi <kingfadzi@gmail.com>
2025-03-17 18:31:13 -0400

    update gradle_helper.py

--- modular/gradle/gradle_helper.py
+++ modular/gradle/gradle_helper.py
@@ -153,8 +153,7 @@ if __name__ == "__main__":
     repo = MockRepo(repo_id, repo_slug)
     session = Session()
     analyzer = GradleHelper()
-    analyzer.logger.setLevel(logging.DEBUG)
-
+    
     try:
         result = analyzer.process_repo(repo_dir, repo, session)
         print(json.dumps(result, indent=2))
