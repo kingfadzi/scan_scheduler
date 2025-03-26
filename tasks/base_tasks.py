72117d75e31c30f93014018eef1a6303ec3c7898
kingfadzi <kingfadzi@gmail.com>
2025-03-26 17:42:54 -0400

    update base_tasks.py

--- tasks/base_tasks.py
+++ tasks/base_tasks.py
@@ -16,14 +16,14 @@ def fetch_repositories_task(payload: dict, batch_size):
 
 
 @task(name="Start Task")
-def start_task(flow_prefix: str) -> str:
+async def start_task(flow_prefix: str) -> str:
     logger = get_run_logger()
     logger.info(f"[{flow_prefix}] Starting flow")
     return flow_prefix
 
 
 @task(name="Clone Repository Task", cache_policy=NO_CACHE)
-def clone_repository_task(repo, sub_dir, run_id):
+async def clone_repository_task(repo, sub_dir, run_id):
     logger = get_run_logger()
     cloning_analyzer = CloningAnalyzer(logger=logger, run_id=run_id)
     result = cloning_analyzer.clone_repository(repo=repo, sub_dir=sub_dir)
@@ -31,21 +31,21 @@ def clone_repository_task(repo, sub_dir, run_id):
 
 
 @task(name="Clean Up Repository Task", cache_policy=NO_CACHE)
-def cleanup_repo_task(repo_dir, run_id):
+async def cleanup_repo_task(repo_dir, run_id):
     logger = get_run_logger()
     cloning_analyzer = CloningAnalyzer(logger=logger, run_id=run_id)
     cloning_analyzer.cleanup_repository_directory(repo_dir)
 
 
 @task(name="Update Processing Status Task", cache_policy=NO_CACHE)
-def update_status_task(repo, run_id):
+async def update_status_task(repo, run_id):
     logger = get_run_logger()
     utils = Utils(logger=logger)
     utils.determine_final_status(repo=repo, run_id=run_id)
 
 
 @task(name="Refresh Views Task")
-def refresh_views_task(flow_prefix: str) -> None:
+async def refresh_views_task(flow_prefix: str) -> None:
     logger = get_run_logger()
     logger.info(f"[{flow_prefix}] Refreshing views")
     utils = Utils(logger = get_run_logger())
