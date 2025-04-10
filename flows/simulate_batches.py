1c68a08c3af8f84f3e35dec3976c90f3ef3e6401
kingfadzi <kingfadzi@gmail.com>
2025-04-10 18:07:13 -0400

    update simulate_batches.py

--- flows/simulate_batches.py
+++
@@ -1,9 +0,0 @@
-from prefect import flow
-from prefect.task_runners import ConcurrentTaskRunner
-
-PER_BATCH_WORKERS = 10
-
-@flow(task_runner=ConcurrentTaskRunner(max_workers=PER_BATCH_WORKERS))
-async def batch_build_profiles_flow(repo_ids: list[str], batch_number: int):
-    for repo_id in repo_ids:
-        build_profile_flow(repo_id=repo_id, batch_number=batch_number)
